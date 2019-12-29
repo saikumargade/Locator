9032ae2d913c9b62aee5d499e823e4c63393ec08
saikumargade <gade.saikumar1@gmail.com>
2019-12-29 19:00:43 +0530

    able to mark multiple locations on map

--- App.js
+++ App.js
@@ -1,31 +1,60 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      {/* <Text>Open up App.js to start working on your app!</Text> */}
-      <MapView
-        style={styles.map}
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
-      >
-        <Marker
-          coordinate={{
+const locations = [
+  {
+    title: "title0",
+    description: "description0",
+    coordinates: {
+      latitude: 37.78825,
+      longitude: -122.4324
+    }
+  },
+  {
+    title: "title1",
+    description: "description1",
+    coordinates: {
+      latitude: 37.77825,
+      longitude: -122.4224
+    }
+  },
+  {
+    title: "title2",
+    description: "description2",
+    coordinates: {
+      latitude: 37.77825,
+      longitude: -122.4524
+    }
+  }
+];
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* <Text>Open up App.js to start working on your app!</Text> */}
+        <MapView
+          style={styles.map}
+          region={{
             latitude: 37.78825,
-            longitude: -122.4324
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
           }}
-          title="title"
-          description="description"
-        />
-      </MapView>
-    </View>
-  );
+        >
+          {locations.map((l, i) => {
+            return (
+              <Marker
+                key={i}
+                coordinate={l.coordinates}
+                title={l.title}
+                description={l.description}
+              />
+            );
+          })}
+        </MapView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
