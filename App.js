421ab2cc1ad525709f33ffaf77186d7eeea3a201
saikumargade <gade.saikumar1@gmail.com>
2019-12-29 16:30:39 +0530

    used MapView from react-native-maps

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <MapView
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    ...StyleSheet.absoluteFillObject,
+    // height: 400,
+    // width: 400,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
   }
 });
